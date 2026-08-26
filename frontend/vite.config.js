b31ed8aa4a07f65ba66af8805982c4d8566e6fd1
Eyang Daniel Eyoh <daniel.eyang.ed@gmail.com>
2026-08-26 22:29:56 +0100

    UPDATE: changed local backend host

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,9 +4,10 @@ import { defineConfig } from "vite";
 export default defineConfig({
   plugins: [react()],
   server: {
+    port: 5173,
     proxy: {
       "/api": {
-        target: "https://players-club-relationship.onrender.com",
+        target: "http://localhost:5000",
         changeOrigin: true,
       },
     },
